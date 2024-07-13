e64cd008ed00969255a8ee44ecb4d65e11cfe831
Talha Hussain <talhahussain349@gmail.com>
2024-07-14 00:40:42 +0500

    .

--- src/Pages/Home/Video/Video.jsx
+++ src/Pages/Home/Video/Video.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import "./video.css"
-import Playvideo from '../../../components/Playvideo/Playvideo'
-import Recommended from '../../../components/Recomended/Recommended'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import "./video.css";
+import Playvideo from "../../../components/Playvideo/Playvideo";
+import Recommended from "../../../components/Recomended/Recommended";
+import { useParams } from "react-router-dom";
 
 const Video = () => {
-
-  const {videoId, categoryId} = useParams();
+  const { videoId, categoryId } = useParams();
   return (
-    <div className='play-container'>
+    <div className="play-container">
       <Playvideo videoId={videoId} />
-      <Recommended categoryId={categoryId}/>
+      <Recommended categoryId={categoryId} />
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
