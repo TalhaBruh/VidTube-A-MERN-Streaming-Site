e3dd06aaf6d789da8cb7e275598fb7aa21582a72
Talha Hussain <talhahussain349@gmail.com>
2024-07-15 19:36:52 +0500

    error removal

--- src/components/Recomended/Recommended.jsx
+++ src/components/Recomended/Recommended.jsx
@@ -1,54 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import "./Recommended.css"
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from "../../assets/thumbnail8.png"
-import { API_KEY, value_converter } from '../../data'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import "./Recommended.css";
+import thumbnail1 from "../../assets/thumbnail1.png";
+import thumbnail2 from "../../assets/thumbnail2.png";
+import thumbnail3 from "../../assets/thumbnail3.png";
+import thumbnail4 from "../../assets/thumbnail4.png";
+import thumbnail5 from "../../assets/thumbnail5.png";
+import thumbnail6 from "../../assets/thumbnail6.png";
+import thumbnail7 from "../../assets/thumbnail7.png";
+import thumbnail8 from "../../assets/thumbnail8.png";
+import { API_KEY, value_converter } from "../../data";
+import { Link } from "react-router-dom";
 
-const Recommended = ({categoryId}) => {
+const Recommended = ({ categoryId }) => {
+  const [apiData, setApiData] = useState([]);
 
-    const [apiData , setApiData] = useState([]);
+  const fetchData = async () => {
+    const relatedVideo_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
 
-    const fetchData = async () => {
-      const relatedVideo_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
-
-      await fetch(relatedVideo_url).then(res => res.json()).then(data => setApiData(data.items))
+    try {
+      const response = await fetch(relatedVideo_url);
+      const data = await response.json();
+      setApiData(data.items);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  };
 
-    useEffect(()=>{
-      fetchData();
-    },[]);
+  useEffect(() => {
+    fetchData();
+  }, [categoryId]);
 
-    console.log(categoryId)
   return (
-    <div className='recommended'> 
-
-    {apiData.map((item , index)=>{
-      return(
-         <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={index} className="side-video-list">
-        <img src={item.snippet.thumbnails.medium.url} alt="" />
-        <div className="vid-info">
+    <div className="recommended">
+      {apiData.map((item, index) => (
+        <Link
+          to={`/video/${item.snippet.categoryId}/${item.id}`}
+          key={index}
+          className="side-video-list"
+        >
+          <img src={item.snippet.thumbnails.medium.url} alt="" />
+          <div className="vid-info">
             <h4>{item.snippet.title}</h4>
-            <p>{item.snippet.channelTitle}}</p>
+            <p>{item.snippet.channelTitle}</p>
             <p>{value_converter(item.statistics.viewCount)} Views</p>
-        </div>
-      </Link>
-      )
-    })}
-      
-
-      
-      
-
-
+          </div>
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Recommended
+export default Recommended;
